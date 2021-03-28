b01932f027121fec8d43c8f13f2077b6ae72e1df
Chua Zi Heng <chuaziheng98@gmail.com>
2021-03-29 01:47:04 +0800

    Updated models

--- app/src/main/java/com/sandbox/chat/models/Buyer.java
+++ app/src/main/java/com/sandbox/chat/models/Buyer.java
@@ -1,5 +1,13 @@
 package com.sandbox.chat.models;
 
-public class Buyer extends User {
+import java.util.ArrayList;
 
+public class Buyer extends User {
+    private ArrayList<Transaction> buyerOrderList;
+
+    public Buyer(String uid, String email, String firebaseToken, ArrayList<Transaction> buyerOrderList) {
+        super(uid, email, firebaseToken);
+        this.buyerOrderList = buyerOrderList;
+    }
 }
+
--- app/src/main/java/com/sandbox/chat/models/Deliverer.java
+++ app/src/main/java/com/sandbox/chat/models/Deliverer.java
@@ -1,5 +1,12 @@
 package com.sandbox.chat.models;
 
-public class Deliverer extends User{
+import java.util.ArrayList;
 
+public class Deliverer extends User{
+    private ArrayList<DelivererOffer> delivererOfferList;
+
+    public Deliverer(String uid, String email, String firebaseToken, ArrayList<DelivererOffer> delivererOfferList) {
+        super(uid, email, firebaseToken);
+        this.delivererOfferList = delivererOfferList;
+    }
 }
