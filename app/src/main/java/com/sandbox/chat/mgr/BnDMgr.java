683c7e5e9c56d3f8921713cd6ebf1ba367e8922c
Cammy Mun <64398971+cammy-mun@users.noreply.github.com>
2021-03-30 15:08:20 +0800

    mvp

--- app/src/main/java/com/sandbox/chat/mgr/BnDMgr.java
+++ app/src/main/java/com/sandbox/chat/mgr/BnDMgr.java
@@ -24,15 +24,15 @@ public class BnDMgr {
      */
 
 
-     public Buyer createBuyer(User user)
-     {
-         //TODO: Call TransactionMgr to get all the transactions
-         return new Buyer(user.getUid(), user.getEmail(), user.getFirebaseToken(), user.getRating() , new LinkedList<Transaction>());
-     }
-
-    public Deliverer createDeliverer(User user)
-    {
-        //TODO: Call TransactionMgr to get all the transactions
-        return new Deliverer(user.getUid(), user.getEmail(), user.getFirebaseToken(), user.getRating(), new LinkedList<Transaction>());
-    }
+//     public static Buyer createBuyer(User user)
+//     {
+//         //TODO: Call TransactionMgr to get all the transactions
+//         return new Buyer(user.getUid(), user.getEmail(), user.getFirebaseToken(), user.getRating() , new LinkedList<Transaction>());
+//     }
+//
+//    public static Deliverer createDeliverer(User user)
+//    {
+//        //TODO: Call TransactionMgr to get all the transactions
+//        return new Deliverer(user.getUid(), user.getEmail(), user.getFirebaseToken(), user.getRating(), new LinkedList<Transaction>());
+//    }
 }
