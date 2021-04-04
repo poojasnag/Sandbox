2a6ba08ff90d9e2e492b2c3d560554b29019839d
Chua Zi Heng <chuaziheng98@gmail.com>
2021-04-04 10:20:42 +0800

    do until placeholder

--- app/src/main/java/com/sandbox/chat/ui/fragments/PlaceOrderFragment.java
+++ app/src/main/java/com/sandbox/chat/ui/fragments/PlaceOrderFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -46,7 +47,6 @@ public class PlaceOrderFragment extends Fragment implements View.OnClickListener
         super.onStart();
         i = getActivity().getIntent();
         submitButton.setOnClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View v) {
                 placeOrderPresenter.onSubmitSuccess();
