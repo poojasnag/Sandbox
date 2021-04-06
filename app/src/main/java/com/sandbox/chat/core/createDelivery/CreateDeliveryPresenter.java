84d3d369ef5746cefbd502f99983cd887f1b8465
poojasnag <poojasri.n@gmail.com>
2021-04-06 17:22:26 +0800

    createdelivery merge w final

---
+++ app/src/main/java/com/sandbox/chat/core/createDelivery/CreateDeliveryPresenter.java
@@ -0,0 +1,49 @@
+package com.sandbox.chat.core.createDelivery;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+import com.sandbox.chat.models.Deliverer;
+import com.sandbox.chat.models.Eatery;
+import com.sandbox.chat.models.User;
+import com.sandbox.chat.ui.activities.CreateDeliveryActivity;
+import com.sandbox.chat.utils.MultiSpinner;
+
+public class CreateDeliveryPresenter implements CreateDeliveryContract.Presenter {
+    private CreateDeliveryContract.View mCreateDeliveryView; // ok
+    // private CreateDeliveryInteractor mCreateDeliveryInteractor; // add next time
+
+    public CreateDeliveryPresenter() {}
+
+    public CreateDeliveryPresenter(CreateDeliveryContract.View createDeliveryView){
+        this.mCreateDeliveryView = createDeliveryView;
+//        mCreateDeliveryInteractor = new CreateDeliveryInteractor(this);
+    }
+
+    // 1. add functions where the presenter calls the view
+    public void onCreateBuyer(User user) {
+        mCreateDeliveryView.createBuyer(user);
+    }
+
+    public void onCreateDeliverer(User user) {
+        mCreateDeliveryView.createDeliverer(user);
+    }
+
+    @Override
+    public void onRecordData(String chosenLoc, double deliveryFee, String cutoffDateTime, String etaDateTime, Eatery eatery, Context context, Deliverer deliverer) {
+        mCreateDeliveryView.recordData(chosenLoc, deliveryFee, cutoffDateTime, etaDateTime, eatery, context, deliverer);
+    }
+
+    @Override
+    public void onSetLocation(Button b, Intent i) {
+        mCreateDeliveryView.setLocation(b,i);
+    }
+
+    @Override
+    public void onSetDeliveryLocations(MultiSpinner deliveryLocSpinner) {
+        mCreateDeliveryView.setDeliveryLocations(deliveryLocSpinner);
+    }
+
+}
--- app/src/main/java/com/sandbox/chat/ui/activities/CreateDeliveryActivity.java
+++ app/src/main/java/com/sandbox/chat/ui/activities/CreateDeliveryActivity.java
@@ -2,11 +2,11 @@ package com.sandbox.chat.ui.activities;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.InputType;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -19,6 +19,8 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -28,11 +30,14 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.sandbox.chat.R;
+import com.sandbox.chat.core.createDelivery.CreateDeliveryPresenter;
 import com.sandbox.chat.mgr.CreateDeliveryMgr;
 import com.sandbox.chat.mgr.DelivererOfferMgr;
 import com.sandbox.chat.models.Deliverer;
 import com.sandbox.chat.models.Eatery;
 import com.sandbox.chat.ui.BottomBarOnClickListener;
+import com.sandbox.chat.ui.fragments.ChooseDelivererFragment;
+import com.sandbox.chat.ui.fragments.CreateDeliveryFragment;
 import com.sandbox.chat.utils.MultiSpinner;
 
 import java.text.SimpleDateFormat;
@@ -49,17 +54,25 @@ import java.util.Map;
  */
 public class CreateDeliveryActivity extends AppCompatActivity {
 
-    Button createDeliveryButton;
-    EditText cutoff_picker;
-    EditText eta_picker;
-    MultiSpinner deliveryLocSpinner;
-    EditText deliveryFeeText;
-    CreateDeliveryMgr createDeliveryController;
-    Button locationDisplay;
-    Intent prevIntent;
-    Eatery curEatery;
-    ArrayList<String> selectedLocations;
+    private Toolbar mToolbar;
+    private Intent i;
+//    Button createDeliveryButton;
+//    EditText cutoff_picker;
+//    EditText eta_picker;
+//    MultiSpinner deliveryLocSpinner;
+//    EditText deliveryFeeText;
+//    CreateDeliveryPresenter createDeliveryPresenter;
+//    Button locationDisplay;
+//    Intent prevIntent;
+//    Eatery curEatery;
 
+    // ok
+    public static void startActivity(Context context){
+        Intent intent = new Intent(context, CreateDeliveryActivity.class);
+        context.startActivity(intent);
+    }
+
+    // ok
     /**
      * Initialize the interface.
      * Consisting of loading the corresponding layout file and binding the on-click listener to the navigation bar.
@@ -70,107 +83,95 @@ public class CreateDeliveryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_delivery);
 
-        createDeliveryButton = findViewById(R.id.create_delivery_send);
-        cutoff_picker = findViewById(R.id.cutoff_datetime);
-        eta_picker = findViewById(R.id.eta_datetime);
-        deliveryLocSpinner = findViewById(R.id.create_delivery_select_location);
-        deliveryFeeText = findViewById(R.id.create_delivery_fee);
-        createDeliveryController = new CreateDeliveryMgr(this);
-
-        locationDisplay = findViewById(R.id.create_delivery_from_location);
-
-    }
-    @Override
-    protected void onStart() {
-
-        super.onStart();
-        prevIntent = getIntent();
-        cutoff_picker.setInputType(InputType.TYPE_NULL);
-        eta_picker.setInputType(InputType.TYPE_NULL);
-        curEatery = ((Eatery)prevIntent.getSerializableExtra("Eatery"));
-        setDeliveryLocations(deliveryLocSpinner);
-        cutoff_picker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDateTimeDialog(cutoff_picker);
-            }
-        });
-
-        eta_picker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDateTimeDialog(eta_picker);
-            }
-        });
-
-
-        createDeliveryButton.setOnClickListener(new View.OnClickListener() {
-
-            //TODO: Check for empty input
-
-            @RequiresApi(api = Build.VERSION_CODES.O)
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(CreateDeliveryActivity.this, "create button clicked", Toast.LENGTH_SHORT).show();
-                
-//                String chosenLoc = deliveryLocSpinner.getSelectedItem().toString();  //TODO: Change to array
-                String deliveryFee = deliveryFeeText.getText().toString();
-                String cutoffDateTime = cutoff_picker. getText().toString();
-                String etaDateTime = eta_picker. getText().toString();
-                Log.e("insideactivity", selectedLocations.toString());
-//                Toast.makeText(v.getContext(), getIntent().getSerializableExtra("user").getClass().getName(),Toast.LENGTH_SHORT).show();
-//                Eatery eatery = new Eatery("", "Koi","", "",  ""); // TODO: create real eatery
-                createDeliveryController.recordData(selectedLocations, Double.parseDouble(deliveryFee), cutoffDateTime, etaDateTime, curEatery, v.getContext(), (Deliverer) getIntent().getSerializableExtra("user"));
-
-            }
-        });
-//        Toast.makeText(CreateDeliveryActivity.this, "text:" + chosenLoc, Toast.LENGTH_SHORT).show();
         final BottomNavigationView bot_bar = findViewById(R.id.create_delivery_bottomNavigationView);
         bot_bar.setOnNavigationItemSelectedListener(new BottomBarOnClickListener(bot_bar));
+        bindViews();
+        init();
 
-        //TODO: Pass selected location to header
-        createDeliveryController.setLocation(locationDisplay, prevIntent);
-
-    }
-    private void showDateTimeDialog(final EditText date_time_in) {
-        final Calendar calendar=Calendar.getInstance();
-        DatePickerDialog.OnDateSetListener dateSetListener=new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                calendar.set(Calendar.YEAR,year);
-                calendar.set(Calendar.MONTH,month);
-                calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
-
-                TimePickerDialog.OnTimeSetListener timeSetListener=new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        calendar.set(Calendar.HOUR_OF_DAY,hourOfDay);
-                        calendar.set(Calendar.MINUTE,minute);
-
-                        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm");
-
-                        date_time_in.setText(simpleDateFormat.format(calendar.getTime()));
-                    }
-                };
-
-                new TimePickerDialog(CreateDeliveryActivity.this,timeSetListener,calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),false).show();
-            }
-        };
-
-        new DatePickerDialog(CreateDeliveryActivity.this,dateSetListener,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show();
+//        createDeliveryButton = findViewById(R.id.create_delivery_send);
+//        cutoff_picker = findViewById(R.id.cutoff_datetime);
+//        eta_picker = findViewById(R.id.eta_datetime);
+//        deliveryLocSpinner = findViewById(R.id.create_delivery_select_location);
+//        deliveryFeeText = findViewById(R.id.create_delivery_fee);
+//        createDeliveryController = new CreateDeliveryMgr(this);
+//
+//        locationDisplay = findViewById(R.id.create_delivery_from_location);
+//
 
     }
-    public void setDeliveryLocations(MultiSpinner deliveryLocSpinner)
-    {
-        selectedLocations = new ArrayList<String>();
-        deliveryLocSpinner.setItems(this.getResources().getStringArray(R.array.deliver_to) ,"Select locations" ,new MultiSpinner.MultiSpinnerListener() {
-            @Override
-            public void onItemsSelected(boolean[] selected) {
-                selectedLocations = deliveryLocSpinner.getAllSelected(selected);
 
-            }
-        });
+    // OK
+    private void bindViews() { mToolbar = (Toolbar) findViewById(R.id.toolbar);}
+
+    // ok
+    private void init(){
+        // set the toolbar
+        setSupportActionBar(mToolbar);
+
+        // set the create delivery screen fragmentn
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout_content_create_delivery,
+                CreateDeliveryFragment.newInstance(),
+                CreateDeliveryFragment.class.getSimpleName());
+        fragmentTransaction.commit();
     }
+
+
+    // remove / shift
+//    @Override
+//    protected void onStart() {
+//
+//        super.onStart();
+//        prevIntent = getIntent();
+//        cutoff_picker.setInputType(InputType.TYPE_NULL);
+//        eta_picker.setInputType(InputType.TYPE_NULL);
+//        curEatery = ((Eatery)prevIntent.getSerializableExtra("Eatery"));
+//        createDeliveryController.setDeliveryLocations(deliveryLocSpinner);
+//
+//        cutoff_picker.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                showDateTimeDialog(cutoff_picker);
+//            }
+//        });
+//
+//        eta_picker.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                showDateTimeDialog(eta_picker);
+//            }
+//        });
+//
+//
+//        createDeliveryButton.setOnClickListener(new View.OnClickListener() {
+//
+//            //TODO: Check for empty input
+//
+//            @RequiresApi(api = Build.VERSION_CODES.O)
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(CreateDeliveryActivity.this, "create button clicked", Toast.LENGTH_SHORT).show();
+//
+//                String chosenLoc = deliveryLocSpinner.getSelectedItem().toString();  //TODO: Change to array
+//                String deliveryFee = deliveryFeeText.getText().toString();
+//                String cutoffDateTime = cutoff_picker. getText().toString();
+//                String etaDateTime = eta_picker. getText().toString();
+//
+////                Toast.makeText(v.getContext(), getIntent().getSerializableExtra("user").getClass().getName(),Toast.LENGTH_SHORT).show();
+////                Eatery eatery = new Eatery("", "Koi","", "",  ""); // TODO: create real eatery
+//                createDeliveryController.recordData(chosenLoc, Double.parseDouble(deliveryFee), cutoffDateTime, etaDateTime, curEatery, v.getContext(), (Deliverer) getIntent().getSerializableExtra("user"));
+//
+//            }
+//        });
+////        Toast.makeText(CreateDeliveryActivity.this, "text:" + chosenLoc, Toast.LENGTH_SHORT).show();
+//        final BottomNavigationView bot_bar = findViewById(R.id.create_delivery_bottomNavigationView);
+//        bot_bar.setOnNavigationItemSelectedListener(new BottomBarOnClickListener(bot_bar));
+//
+//        //TODO: Pass selected location to header
+//        createDeliveryController.setLocation(locationDisplay, prevIntent);
+//
+//    }
+
 
 }
 
